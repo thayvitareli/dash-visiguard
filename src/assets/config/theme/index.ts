4f8ecbbb5079cc00b6f815387715027a41cdd5be
thayvitareli <thainyvitareli@hotmail.com>
2024-05-18 12:38:38 -0300

    chore: initial theme config

---
+++ src/assets/config/theme/index.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+  primary: "#8FAADC",
+  second: "#4472C4",
+};
+
+export const TextSize = {
+  heading: "36px",
+  sm: "16px",
+  md: "26px",
+};
+
+export const ButtonColor = {
+  primary: "#4472C4",
+};
