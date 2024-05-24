dd12c82ec3bc8368b7d8ebafe20539f76c44f2df
thayvitareli <thainyvitareli@hotmail.com>
2024-05-24 16:55:15 -0300

    chore: data config

---
+++ src/assets/config/company-departaments/index.ts
@@ -0,0 +1,26 @@
+export const DepartamentsOptions = [
+  { value: 1, label: "Rh" },
+  { value: 2, label: "Administrativo" },
+  { value: 3, label: "Produção" },
+  { value: 4, label: "Logística" },
+  { value: 5, label: "Expedição" },
+  { value: 6, label: "TI" },
+];
+
+export const PositionsOptions = [
+  { value: 1, label: "Auxiliar Rh" },
+  { value: 2, label: "Auxiliar Administrativo" },
+  { value: 3, label: "Auxiliar Faturamento" },
+  { value: 4, label: "Auxiliar de Produção" },
+  { value: 5, label: "Auxiliar de Logística" },
+  { value: 6, label: "Auxiliar de Expedição" },
+  { value: 7, label: "Gerente de Rh" },
+  { value: 8, label: "Gerente de Produção" },
+  { value: 9, label: "Gerente de Logística" },
+  { value: 10, label: "Gerente de  Expedição" },
+  { value: 11, label: "Gerente de TI" },
+  { value: 12, label: "Diretor de Produção" },
+  { value: 14, label: "Diretor de Logística" },
+  { value: 15, label: "Diretor de Expedição" },
+  { value: 16, label: "Diretor de TI" },
+];
